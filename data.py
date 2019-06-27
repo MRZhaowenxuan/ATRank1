2d7456405a691b44e6ec974d44b337c9b6c0ae19
MRZhao <2110637853@qq.com>
2019-06-27 17:18:15 +0800

    2019/6/27

--- data.py
+++ data.py
@@ -1,4 +1,6 @@
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
 
 with open('../shop_data/remap.pkl', 'rb') as f:
   reviews_df = pickle.load(f)
@@ -7,9 +9,22 @@ with open('../shop_data/remap.pkl', 'rb') as f:
   user_count, item_count, cate_count, action_count, example_count = pickle.load(f)
 
 print('user_count:%d,item_count:%d,example_count:%d' % (user_count, item_count,  example_count))
+
+user_num = np.array(np.arange(user_count))
+hist_length = []
 for reviewerID, hist in reviews_df.groupby('user_id'):
   hist_list = hist['item_id'].tolist()
   print('%d:%d' % (reviewerID, len(hist_list)))
+  hist_length.append(len(hist_list))
+
+fig = plt.figure()
+fig, ax = plt.subplots()
+ax = fig.add_subplot(3, 2, 1)
+ax.scatter(user_num, hist_length)
+ax.set_xlabel('user_id')
+ax.set_ylabel('hist_length')
+plt.show()
+
 
 action0 = 0
 action1 = 0
@@ -26,3 +41,40 @@ for action in reviews_df['item_id']:
         action3 += 1
 
 print('click:%d,add-to-cart:%d,purchase:%d,add-to-favourite:%d' % (action0, action1, action2, action3))
+
+# 创建一个点数为 8 x 6 的窗口, 并设置分辨率为 80像素/每英寸
+plt.figure(figsize=(8, 6), dpi=80)
+
+# 再创建一个规格为 1 x 1 的子图
+plt.subplot(1, 1, 1)
+
+# 柱子总数
+N = 4
+# 包含每个柱子对应值的序列
+values = (action0, action1, action2, action3)
+
+# 包含每个柱子下标的序列
+index = np.arange(N)
+
+# 柱子的宽度
+width = 0.35
+
+# 绘制柱状图, 每根柱子的颜色为紫罗兰色
+p2 = plt.bar(index, values, width, label="rainfall", color="#87CEFA")
+
+# 设置横轴标签
+plt.xlabel('action_type')
+# 设置纵轴标签
+plt.ylabel('action_type_num')
+
+# 添加标题
+plt.title('distribution of actions')
+
+# 添加纵横轴的刻度
+plt.xticks(index, ('click', 'add-to-cart', 'purchase', 'add-to-favourite'))
+plt.yticks(np.arange(0, 81, 10))
+
+# 添加图例
+plt.legend(loc="upper right")
+
+plt.show()
